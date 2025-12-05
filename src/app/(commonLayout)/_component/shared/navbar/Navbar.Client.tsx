d4ce695ef94e7dfdf58cddb3058ade53e9b99f26
istiak19 <istiak15-14128@diu.edu.bd>
2025-12-05 10:25:41 +0600

    explore page created

--- src/app/(commonLayout)/_component/shared/navbar/Navbar.Client.tsx
+++ src/app/(commonLayout)/_component/shared/navbar/Navbar.Client.tsx
@@ -20,7 +20,7 @@ export default function NavbarClient({ isAuthenticated, role }: NavbarClientProp
     const navigationLinks = [
         { href: "/", label: "Home" },
         { href: "/about", label: "About" },
-        { href: "/consultation", label: "Consultation" },
+        { href: "/explore", label: "Explore" },
         { href: "/pricing", label: "Pricing" },
     ];
 
